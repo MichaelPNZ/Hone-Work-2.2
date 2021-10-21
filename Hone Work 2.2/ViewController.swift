99de41f418228c3f504de6beb20507c00165a382
MichaelPNZ <85676466+MichaelPNZ@users.noreply.github.com>
2021-10-21 23:14:00 +0300

    Initial Commit

---
+++ Hone Work 2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hone Work 2.2
+//
+//  Created by Михаил Позялов on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
