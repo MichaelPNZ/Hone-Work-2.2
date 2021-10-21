bff2255d39d7c2fbdafa86ad575458d9cc4aba92
MichaelPNZ <85676466+MichaelPNZ@users.noreply.github.com>
2021-10-22 00:15:34 +0300

    Add Outlets

--- Hone Work 2.2/ViewController.swift
+++ Hone Work 2.2/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var viewColorOutlet: UIView!
+    
+    @IBOutlet var redValueOutlet: UILabel!
+    @IBOutlet var greenValueOutlet: UILabel!
+    @IBOutlet var blueValueOutlet: UILabel!
+    
+    @IBOutlet var redSliderOutlet: UISlider!
+    @IBOutlet var greenSliderOutlet: UISlider!
+    @IBOutlet var blueSliderOutlet: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSliderOutlet.minimumValue = 0
+        redSliderOutlet.maximumValue = 1
+        redSliderOutlet.value = 0
+        redSliderOutlet.minimumTrackTintColor = .red
+        
+        greenSliderOutlet.minimumValue = 0
+        greenSliderOutlet.maximumValue = 1
+        greenSliderOutlet.value = 0
+        greenSliderOutlet.minimumTrackTintColor = .green
+        
+        blueSliderOutlet.minimumValue = 0
+        blueSliderOutlet.maximumValue = 1
+        blueSliderOutlet.value = 0
+        blueSliderOutlet.minimumTrackTintColor = .blue
+        
+        redValueOutlet.text = String(redSliderOutlet.value)
+        greenValueOutlet.text = String(greenSliderOutlet.value)
+        blueValueOutlet.text = String(blueSliderOutlet.value)
+        
     }
-
-
+    @IBAction func redValueAction(_ sender: Any) {
+    }
+    
+    @IBAction func greenValueAction(_ sender: Any) {
+    }
+    @IBAction func blueValueAction(_ sender: Any) {
+    }
+    
 }
 
